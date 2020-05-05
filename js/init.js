6bfa00e36e84eab343a7b748420c0a44b6bbbd39
Andrej Podgorsek <andrej.podgorsek@is2.de>
2020-05-05 12:12:45 +0200

    First commit

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+  $('input#input_text, textarea#textarea2').characterCounter();
+});
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
